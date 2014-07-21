2b82303e74f3e2c62e7e1623d0c7d96e6f81ce6e
Marco La Rosa <m@lr.id.au>
2014-07-21 15:47:33 +1000

    initialise cassandra connection once at startup

--- essos/__init__.py
+++ essos/__init__.py
@@ -1,12 +1,33 @@
 from pyramid.config import Configurator
 from pyramid.paster import setup_logging
 
+from config import Config as appConfig
+
+from Connectors import CassandraBackend
+
+def init_cassandra_connection(conf):
+    arguments = {
+        'nodes': conf['nodes'],
+        'user': conf['user'],
+        'pass': conf['pass'],
+        'keyspace': conf['keyspace']
+    }
+    c = CassandraBackend(arguments)
+    return c.session
+ 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
     setup_logging(global_config['__file__'])
+
+    # initialise a connection to cassandra on startup and store the region
+    #  in the registry which will be injected into each request
+    conf = appConfig(config.registry.settings.get('app.config'))
+    config.registry.app_config = conf.app_config
+    config.registry.app_config['cassandra']['session'] = init_cassandra_connection(config.registry.app_config['cassandra'])
+
     config.include('pyramid_mako')
     config.add_static_view('static', 'static', cache_max_age=3600)
 
