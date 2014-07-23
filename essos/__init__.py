574b988cff3df1061a4afc6b9de1c6f6ada9df0a
Marco La Rosa <m@lr.id.au>
2014-07-23 16:38:42 +1000

    add logout route

--- essos/__init__.py
+++ essos/__init__.py
@@ -33,6 +33,7 @@ def main(global_config, **settings):
 
     config.add_route('home', '/')
     config.add_route('admin', '/admin')
+    config.add_route('logout', '/logout')
     config.add_route('health_check', '/health-check')
     config.add_route('login_staff', '/login/staff')
     config.add_route('login_google', '/login/google')
