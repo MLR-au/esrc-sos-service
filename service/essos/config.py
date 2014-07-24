7a52e0d826da984b3b5566eed51466403d1d5c25
Marco La Rosa <m@lr.id.au>
2014-07-24 17:32:36 +1000

    refactor and add appconfig reader class

--- service/essos/config.py
+++ service/essos/config.py
@@ -3,13 +3,25 @@ import os
 import sys
 import os.path
 import ConfigParser
+import collections
 
 from pyramid.httpexceptions import HTTPBadRequest
 
 import logging
 log = logging.getLogger('essos')
 
-class Config:
+
+class ConfigBase:
+    def __init__(self):
+        pass
+
+    def get(self, section, param, aslist=False):
+        data = self.cfg.get(section, param) if (self.cfg.has_section(section) and self.cfg.has_option(section, param)) else None
+        if aslist:
+            return [ d.strip() for d in data.split(',') ]
+        return data
+
+class Config(ConfigBase):
 
     def __init__(self, conf):
         """
@@ -34,28 +46,40 @@ class Config:
 
         self.app_config = {
             'general': {
-                'admins': self.get('General', 'admins', True),
-                'session_lifetime': self.get('General', 'session_lifetime'),
-                'cookie.domain': self.get('General', 'cookie.domain'),
-                'cookie.secure': self.get('General', 'cookie.secure'),
+                'admins': self.get('General', 'admins', aslist=True),
+                'admin.app': self.get('General', 'admin.app'),
+                'apps': self.get('General', 'apps'),
+                'session.lifetime': self.get('General', 'session.lifetime'),
             },
             'ldap': {
-                'servers': self.get('LDAP', 'ldap_servers', True),
-                'base': self.get('LDAP', 'search_base'),
-                'binduser': self.get('LDAP', 'bind_user'),
-                'bindpass': self.get('LDAP', 'bind_pass')
+                'servers': self.get('LDAP', 'ldap.servers', aslist=True),
+                'base': self.get('LDAP', 'search.base'),
+                'binduser': self.get('LDAP', 'bind.user'),
+                'bindpass': self.get('LDAP', 'bind.pass')
             },
             'cassandra': {
                 'user': self.get('CASSANDRA', 'user'),
                 'pass': self.get('CASSANDRA', 'pass'),
-                'nodes': self.get('CASSANDRA', 'nodes', True),
+                'nodes': self.get('CASSANDRA', 'nodes', aslist=True),
                 'keyspace': self.get('CASSANDRA', 'keyspace')
             }
         }
 
+class AppsConfig(ConfigBase):
+    def __init__(self, conf):
+        self.cfg = ConfigParser.SafeConfigParser()
+        try:
+            self.cfg.read(conf)
+        except ConfigParser.ParsingError:
+            log.error('Config file parsing errors')
+            log.error(sys.exc_info()[1])
+            sys.exit()
 
-    def get(self, section, param, aslist=False):
-        data = self.cfg.get(section, param) if (self.cfg.has_section(section) and self.cfg.has_option(section, param)) else None
-        if aslist:
-            return [ d.strip() for d in data.split(',') ]
-        return data
+    def load(self):
+        conf = collections.namedtuple('appsconf', [ 'name', 'url' ])
+        return conf(self.get('General', 'name'), self.get('General', 'url'))
+
+
+
+
+
