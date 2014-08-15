0448d38d36725b0b7d347f313a1a7a16378ec9e1
Marco La Rosa <m@lr.id.au>
2014-08-15 17:35:01 +1000

    replace cassandra with mongo, refactor and cleanup

--- essos/common.py
+++ essos/common.py
@@ -13,47 +13,48 @@ log = logging.getLogger('essos')
 import ast
 import os
 from urlparse import urlparse
-from models import CSession, ORM
+from connectors import MongoDBConnection as mdb
 
 from config import AppsConfig
 
-def move_the_user_on(request, r, data):
+def set_cookie(request, token):
+    request.response.set_cookie('EAT', str(token),
+        domain=request.registry.app_config['general']['cookie.domain'], path='/',
+        secure=ast.literal_eval(request.registry.app_config['general']['cookie.secure']))
+
+def delete_cookie(request):
+    request.response.delete_cookie('EAT', path='/', domain=request.registry.app_config['general']['cookie.domain'])
+
+def move_the_user_on(request, r, token):
     # send the user on: either back to where they came
     #  from (if r is not None) or on to their profile page
-    request.response.set_cookie('EAT', str(data.token),
-        domain=request.registry.app_config['general']['cookie.domain'], path='/',
-        secure=ast.literal_eval(request.registry.app_config['general']['cookie.secure']), httponly=True)
+    set_cookie(request, token)
 
-    if not data.is_admin:
-        if r and compare(r, request.registry.app_config['general']['admin.app']):
-            log.debug("Non admin user %s trying to access admin console via redirect param." % data.username )
-            raise HTTPFound('/')
-
-    if data.is_admin:
-        raise HTTPFound("%s?s=%s" % (request.registry.app_config['general']['admin.app'], data.code), headers=request.response.headers)
-    elif r and not compare(request.registry.app_config['general']['admin.app'], r):
-        raise HTTPFound("%s?s=%s" % (r, data.code), headers=request.response.headers)
+    if r is not None and validate_app_redirect(request, r):
+        raise HTTPFound("%s" % r, headers=request.response.headers)
     else:
         raise HTTPFound('/profile', headers=request.response.headers)
 
 def validate_app_redirect(request, r):
     authed_app = False
     if r is not None:
-        if compare(r, request.registry.app_config['general']['admin.app']):
-            authed_app = True
-        else:
-            app_configs = os.path.join(os.path.dirname(request.registry.settings['app.config']), request.registry.app_config['general']['apps'])
-            apps = {}
-            for f in os.listdir(app_configs):
-                c = AppsConfig(os.path.join(app_configs, f))
-                d = c.load()
-                apps[d.name] = d.url
-         
-            for k, v in apps.items():
-                if compare(r, v):
-                    authed_app = True
+        apps = get_app_data(request)
+    
+        for app in apps:
+            if compare(r, app.url):
+                authed_app = True
 
     return authed_app
+
+def get_app_data(request):
+    app_configs = os.path.join(os.path.dirname(request.registry.settings['app.config']), request.registry.app_config['general']['apps'])
+    apps = []
+    for f in os.listdir(app_configs):
+        c = AppsConfig(os.path.join(app_configs, f))
+        d = c.load()
+        apps.append(d)
+    return apps
+
 
 def compare(a, b):
     # expects a couple of urls - urlparse will be used to 
@@ -66,20 +67,30 @@ def compare(a, b):
         return False
 
 def check_user_known(request):
+    log.debug("%s: Method: check_user_known" % request.client_addr)
     # is there a token in the request?
     token = request.cookies.get('EAT')
     if token is None:
+        log.debug("%s: No existing token found" % request.client_addr)
         return False
 
     # is the token still valid?
-    session = CSession(request)
-    orm = ORM(session)
-    data = orm.query('session_by_token',
-        where = [ "\"token\" = %s" % token ]
-    )
-    if not data:
+    db = mdb(request)
+    user = db.session.find_one(token=token)
+    if user is not None:
+        return user
+    else:
         return False
 
-    # if we get to here then the user is known
-    #   so we return the data we have about them.
-    return data[0]
+def lockout_ip(request):
+    db = mdb(request)
+    doc = db.lockout.find_one( { 'ip': request.client_addr })
+    if doc is not None:
+        pass
+    else:
+        db.lockout.insert({
+            'ip':  request.client_addr,
+            'attempts': [ datetime.utcnow() ]
+        })
+    
+    
